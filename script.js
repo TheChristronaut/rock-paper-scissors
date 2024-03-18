693e7eda523fb1bb3e4c136d82a9180e4c70a6dc
Zach <zswiger1997@gmail.com>
2024-03-18 13:03:44 -0500

    Add loop and score system

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-let playerChoice = prompt("Please type rock, paper, or scissors").toLowerCase();
-
-/* function getPlayerChoice () {
-    let playerChoice = prompt("Please type rock, paper, or scissors").toLowerCase();
-    if (playerChoice == "rock") {
-        return "rock";
-    }
-    if (playerChoice == "paper") {
-        return "paper";
-    }
-    if (playerChoice == "scissors") {
-        return "scissors";
-    } else if (playerChoice != "rock" || "paper" || "scissors") {
-        return "YOU FOOL!"
-    }
-}
-
-console.log("Player Selected:", getPlayerChoice()) */
-
-console.log("Player Selected:", playerChoice)
-
 function getComputerChoice() {
     let computerNumber = Math.floor(Math.random() * 3)
     if (computerNumber == 0) {
@@ -32,29 +11,48 @@ function getComputerChoice() {
     }
 }
 
-console.log("Computer Selected:", getComputerChoice())
+function playGame(){
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i <5; i++) {
+        function playRound(playerSelection, computerSelection) {
+            if (
+                (playerSelection == "rock" && computerSelection == "paper") ||
+                (playerSelection == "paper" && computerSelection == "scissors") ||
+                (playerSelection == "scissors" && computerSelection == "rock")
+                ) {
+                    computerScore++;
+                    return ("You lose! " + computerSelection + " beats " + playerSelection)
+            }
+            if (
+                (playerSelection == "rock" && computerSelection == "scissors") ||
+                (playerSelection == "paper" && computerSelection == "rock") ||
+                (playerSelection == "scissors" && computerSelection == "paper")
+                ) {
+                    playerScore++;
+                    return ("You win! " + playerSelection + " beats " + computerSelection)
+            }
+            if (
+                (playerSelection == "rock" && computerSelection == "rock") ||
+                (playerSelection == "paper" && computerSelection == "paper") ||
+                (playerSelection == "scissors" && computerSelection == "scissors")) {
+                    return ("It's a tie!")
+            }
+        }
+        const playerSelection = prompt ("Please type rock, paper or scissors").toLowerCase();
+        console.log("Player Selected:", playerSelection)
+        const computerSelection = getComputerChoice();
+        console.log("Computer Selected:", computerSelection)
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("Player Score:", playerScore)
+        console.log("Computer Score:", computerScore)
+    }
+    if (playerScore > computerScore) {
+        alert ("You Win! Congrats!")
+    }
+    if (computerScore > playerScore) {
+        alert ("You Lose! Sorry!")
+    }
+}
 
-function playRound(playerSelection, computerSelection) {
-    if (
-        (playerSelection == "rock" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "scissors") ||
-        (playerSelection == "scissors" && computerSelection == "rock")) {
-            return ("You lose! " + computerSelection + " beats " + playerSelection)
-    }
-    if (
-        (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "paper" && computerSelection == "rock") ||
-        (playerSelection == "scissors" && computerSelection == "paper")) {
-            return ("You win! " + playerSelection + " beats " + computerSelection)
-    }
-    if (
-        (playerSelection == "rock" && computerSelection == "rock") ||
-        (playerSelection == "paper" && computerSelection == "paper") ||
-        (playerSelection == "scissors" && computerSelection == "scissors")) {
-            return ("It's a tie!")
-    }
-  }
-  
-const playerSelection = playerChoice;
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+playGame();
