3ab966a6af2fdf9b2f49f971d367ddfa2e181d86
Zach <zswiger1997@gmail.com>
2024-03-18 11:34:53 -0500

    Add player prompt to make choice

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function getPlayerChoice () {
+    let playerChoice = prompt("Please type rock, paper, or scissors").toLowerCase();
+    if (playerChoice == "rock") {
+        return "rock";
+    }
+    if (playerChoice == "paper") {
+        return "paper";
+    }
+    if (playerChoice == "scissors") {
+        return "scissors";
+    } else if (playerChoice != "rock" || "paper" || "scissors") {
+        return "YOU FOOL!"
+    }
+}
+
+console.log("Player Selected:", getPlayerChoice())
+
 function getComputerChoice() {
     let computerNumber = Math.floor(Math.random() * 3)
     if (computerNumber == 0) {
