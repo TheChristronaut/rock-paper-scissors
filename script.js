bd3afc154641028d638c7f5094e126e905d37234
Zach <zswiger1997@gmail.com>
2024-03-19 17:37:07 -0500

    Add "lizard" and "Spock" as choices

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    let computerNumber = Math.floor(Math.random() * 3)
+    let computerNumber = Math.floor(Math.random() * 5)
     if (computerNumber == 0) {
         return "rock";
     }
@@ -9,37 +9,149 @@ function getComputerChoice() {
     if (computerNumber == 2) {
         return "scissors";
     }
+    if (computerNumber == 3) {
+        return "lizard";
+    }
+    if (computerNumber == 4) {
+        return "spock";
+    }
 }
 
 function playGame(){
     let playerScore = 0;
     let computerScore = 0;
-    for (let i = 0; i <5; i++) {
+    for (let i = 0; i < 5; i++) {
         function playRound(playerSelection, computerSelection) {
             if (
-                (playerSelection == "rock" && computerSelection == "paper") ||
-                (playerSelection == "paper" && computerSelection == "scissors") ||
+                (playerSelection == "paper" && computerSelection == "scissors")
+                ) {
+                    computerScore++;
+                    return ("You lose! Scissors cuts Paper")
+            }
+            if (
+                (playerSelection == "rock" && computerSelection == "paper")
+                ) {
+                    computerScore++;
+                    return ("You lose! Paper covers Rock")
+            }
+            if (
+                (playerSelection == "lizard" && computerSelection == "rock")
+                ) {
+                    computerScore++;
+                    return ("You lose! Rock crushes Lizard")
+            }
+            if (
+                (playerSelection == "spock" && computerSelection == "lizard")
+                ) {
+                    computerScore++;
+                    return ("You lose! Lizard poisons Spock")
+            }
+            if (
+                (playerSelection == "scissors" && computerSelection == "spock")
+                ) {
+                    computerScore++;
+                    return ("You lose! Spock smashes Scissors")
+            }
+            if (
+                (playerSelection == "lizard" && computerSelection == "scissors")
+                ) {
+                    computerScore++;
+                    return ("You lose! Scissors decapitates Lizard")
+            }
+            if (
+                (playerSelection == "paper" && computerSelection == "Lizard")
+                ) {
+                    computerScore++;
+                    return ("You lose! Lizard eats Paper")
+            }
+            if (
+                (playerSelection == "spock" && computerSelection == "paper")
+                ) {
+                    computerScore++;
+                    return ("You lose! Paper disproves Spock")
+            }
+            if (
+                (playerSelection == "rock" && computerSelection == "spock")
+                ) {
+                    computerScore++;
+                    return ("You lose! Spock vaporizes Rock")
+            }
+            if (
                 (playerSelection == "scissors" && computerSelection == "rock")
                 ) {
                     computerScore++;
-                    return ("You lose! " + computerSelection + " beats " + playerSelection)
+                    return ("You lose! Rock crushes Scissors")
             }
             if (
-                (playerSelection == "rock" && computerSelection == "scissors") ||
-                (playerSelection == "paper" && computerSelection == "rock") ||
                 (playerSelection == "scissors" && computerSelection == "paper")
                 ) {
                     playerScore++;
-                    return ("You win! " + playerSelection + " beats " + computerSelection)
+                    return ("You win! Scissors cuts Paper")
+            }
+            if (
+                (playerSelection == "paper" && computerSelection == "rock")
+                ) {
+                    playerScore++;
+                    return ("You win! Paper covers Rock")
+            }
+            if (
+                (playerSelection == "rock" && computerSelection == "lizard")
+                ) {
+                    playerScore++;
+                    return ("You win! Rock crushes Lizard")
+            }
+            if (
+                (playerSelection == "lizard" && computerSelection == "spock")
+                ) {
+                    playerScore++;
+                    return ("You win! Lizard poisons Spock")
+            }
+            if (
+                (playerSelection == "spock" && computerSelection == "scissors")
+                ) {
+                    playerScore++;
+                    return ("You win! Spock smashes Scissors")
+            }
+            if (
+                (playerSelection == "scissors" && computerSelection == "lizard")
+                ) {
+                    playerScore++;
+                    return ("You win! Scissors decapitates Lizard")
+            }
+            if (
+                (playerSelection == "lizard" && computerSelection == "paper")
+                ) {
+                    playerScore++;
+                    return ("You win! Lizard eats Paper")
+            }
+            if (
+                (playerSelection == "paper" && computerSelection == "spock")
+                ) {
+                    playerScore++;
+                    return ("You win! Paper disproves Spock")
+            }
+            if (
+                (playerSelection == "spock" && computerSelection == "rock")
+                ) {
+                    playerScore++;
+                    return ("You win! Spock vaporizes Rock")
+            }
+            if (
+                (playerSelection == "rock" && computerSelection == "scissors")
+                ) {
+                    playerScore++;
+                    return ("You win! Rock crushes Scissors")
             }
             if (
                 (playerSelection == "rock" && computerSelection == "rock") ||
                 (playerSelection == "paper" && computerSelection == "paper") ||
-                (playerSelection == "scissors" && computerSelection == "scissors")) {
+                (playerSelection == "scissors" && computerSelection == "scissors") ||
+                (playerSelection == "lizard" && computerSelection == "lizard") ||
+                (playerSelection == "spock" && computerSelection == "spock")) {
                     return ("It's a tie!")
             }
         }
-        const playerSelection = prompt ("Please type rock, paper or scissors").toLowerCase();
+        const playerSelection = prompt ("Please type rock, paper, scissors, lizard or Spock").toLowerCase();
         console.log("Player Selected:", playerSelection)
         const computerSelection = getComputerChoice();
         console.log("Computer Selected:", computerSelection)
